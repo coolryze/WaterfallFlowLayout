b5407337783c338a165c67d6267417f9ac282378
heyuze <ryzehe@gmail.com>
2018-06-11 17:57:24 +0800

    update demo

--- WaterfallFlowLayoutDemo/WaterfallFlowLayoutDemo/ViewController.swift
+++ WaterfallFlowLayoutDemo/WaterfallFlowLayoutDemo/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
+func RandomColor() -> UIColor {
+  return UIColor(red: CGFloat(arc4random() % 256) / 255, green: CGFloat(arc4random() % 256) / 255, blue: CGFloat(arc4random() % 256) / 255, alpha: 1)
+}
+
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    let waterfallFlowLayout = WaterfallFlowLayout()
+    waterfallFlowLayout.delegate = self
+    let collectionView = UICollectionView(frame: view.frame, collectionViewLayout: waterfallFlowLayout)
+    collectionView.dataSource = self
+    collectionView.backgroundColor = .white
+    collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+    
+    view.addSubview(collectionView)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
+// MARK: - WaterfallFlowLayoutDelegate
+extension ViewController: WaterfallFlowLayoutDelegate {
+  
+  func waterfallFlowLayout(_ layout: WaterfallFlowLayout, heightForItemAtIndexPath indexPath: IndexPath, itemWidth: CGFloat) -> CGFloat {
+    return CGFloat(arc4random_uniform(200) + 100)
+  }
+  
+  func columnCountInWaterfallFlowLayout(_ layout: WaterfallFlowLayout) -> Int {
+    return 3
+  }
+  
+  func columnSpacingInWaterfallFlowLayout(_ layout: WaterfallFlowLayout) -> CGFloat {
+    return 15
+  }
+  
+  func rowSpacingInWaterfallFlowLayout(_ layout: WaterfallFlowLayout) -> CGFloat {
+    return 15
+  }
+  
+  func edgeInsetsInWaterfallFlowLayout(_ layout: WaterfallFlowLayout) -> UIEdgeInsets {
+    return UIEdgeInsets(top: 15, left: 0, bottom: 15, right: 0)
+  }
+}
+
+// MARK: - UICollectionViewDataSource
+extension ViewController: UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 100
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+    cell.backgroundColor = RandomColor()
+    return cell
+  }
+}
